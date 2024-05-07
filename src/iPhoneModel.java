0ed746e71dd53575ccd3f2fc2f512a24adf8111d
Vagner Carvalho <vagnercode@gmail.com>
2024-05-07 18:41:34 -0300

    INICIAL

---
+++ src/iPhoneModel.java
@@ -0,0 +1,38 @@
+class iPhoneModel implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    // Implementações dos métodos das interfaces
+    public void tocar() {
+        // Implementação
+    }
+
+    public void pausar() {
+        // Implementação
+    }
+
+    public void selecionarMusica(String musica) {
+        // Implementação
+    }
+
+    public void ligar(String numero) {
+        // Implementação
+    }
+
+    public void atender() {
+        // Implementação
+    }
+
+    public void iniciarCorreioVoz() {
+        // Implementação
+    }
+
+    public void exibirPagina(String url) {
+        // Implementação
+    }
+
+    public void adicionarNovaAba(String url) {
+        // Implementação
+    }
+
+    public void atualizarPagina() {
+        // Implementação
+    }
+}
